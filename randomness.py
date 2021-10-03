80018812704a4e71eef804636d8bfc8c3730da04
Akanksha Makkar <akanksha.azure@gmail.com>
2021-10-03 21:22:42 +0200

    function to print random numbers and calculate average

--- randomness.py
+++ randomness.py
@@ -4,15 +4,15 @@ def randomness():
     return(random.randint(1, 6))
 
 def average():
-    n = input("Enter a number of dice to throw: ")
+    n = input("Enter a number of dice to throw: ")    #take input from user for number of iterations
     n1 = int(n)
     sum = 0
     print("\n")
     for i in range(1,n1+1):
-        sum = randomness()+sum
+        sum = randomness()+sum                         #calling the random number function and calc sum of the throws
         print("Die %d: %d" %(i,randomness()))
     print("\n")
-    avg = sum/6
+    avg = sum/6                                        # calculating average of the numbers got over different throws
     print("Average : %.1f" %(avg))
 
 # average()
