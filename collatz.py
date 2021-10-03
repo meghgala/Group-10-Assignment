9f5917eedc22a04b5aa5008d6737b3c546f24e53
SagarTB <sagar.bhat97@gmail.com>
2021-10-03 21:28:25 +0200

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -5,11 +5,11 @@ def collatz():
     print("\n")
     print("%d" %(n))
     while (n > 1):
-        if(n % 2 == 0):
+        if(n % 2 == 0):  #If the number is divisible by 2, perform n/2
             n = n/2
             print("%d" %(n))
         else:
-            n = (3*n) + 1
+            n = (3*n) + 1  #If the number is not divisible by 2, perform 3n+1
             print("%d" %(n))
 
 
